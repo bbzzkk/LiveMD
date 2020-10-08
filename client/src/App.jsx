696088670825f00ea0701455a4be30ca0594e41f
kwak-bs <iam-bs@naver.com>
2020-10-08 15:18:52 +0900

    FEATURE : Markdown 문서 작성 기능 추가
    why
    - 핵심 기능 구현
    
    What
    - toast-ui 적용
    - 마크다운 에디터 적용

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/react-editor';
 
 const App = () => {
-  return <div>CRA ESLint Prettier 설정 완료.</div>;
+  return (
+    <div>
+      CRA ESLint Prettier 설정 완료.
+      <Editor previewStyle="vertical" usageStatistics={false} />
+    </div>
+  );
 };
 
 export default App;
