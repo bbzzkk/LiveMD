c236c8393910788c9094d589e7801b87bbd8759f
soyoung <so_young_jung@naver.com>
2020-10-17 14:27:28 +0900

    테스트 코드 추가

--- src/test/java/com/livemd/document/api/DocumentsApiControllerTest.java
+++ src/test/java/com/livemd/document/api/DocumentsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.livemd.document.api;
 import com.livemd.document.domain.entity.Documents;
 import com.livemd.document.domain.repository.DocumentsRepository;
 import com.livemd.document.dto.DocumentsSaveRequestDto;
+import com.livemd.document.dto.DocumentsTitleUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -58,6 +61,38 @@ public class DocumentsApiControllerTest {
         List<Documents> all = repository.findAll();
         assertThat(all.get(0).getOwner()).isEqualTo(owner);
         assertThat(all.get(0).getTitle()).isEqualTo(title);
+
+    }
+
+    @Test
+    public void updateDocumentsTitle() throws Exception{
+        //given
+        Documents savedDocuments = repository.save(Documents.builder()
+                .owner("owner")
+                .title("title")
+                .content("content")
+                .build());
+
+        Long updateId = savedDocuments.getId();
+        String expectedTitle = "title2";
+
+        DocumentsTitleUpdateRequestDto requestDto = DocumentsTitleUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/documents/" + updateId;
+
+        HttpEntity<DocumentsTitleUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Documents> all = repository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
 
     }
 }
