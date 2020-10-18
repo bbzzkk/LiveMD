de08d6cf25736cd4efa80c783c216eb61dda0a18
i_am_play__ <52230415+Joylish@users.noreply.github.com>
2020-10-19 00:55:03 +0900

    Revert "Develop document"

--- server/spring-document/src/main/java/com/livemd/document/domain/repository/DocumentsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.livemd.document.domain.repository;
-
-import com.livemd.document.domain.entity.Documents;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentsRepository extends JpaRepository<Documents, Long> {
-
-}
---
+++ server/spring-social/src/main/java/com/livemd/springsocial/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.livemd.springsocial.repository;
+
+import com.livemd.springsocial.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
