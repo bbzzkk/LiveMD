518e7a3f81a93fcdfbe223f469eedcc329f83b80
soyoung <so_young_jung@naver.com>
2020-10-20 18:00:01 +0900

    LMD-99 TEST : 문서 생성 생성/수정시간 JPA Auditing 테스트

--- server/spring-document/src/main/java/com/livemd/document/domain/BaseTimeEntity.java
+++ server/spring-document/src/main/java/com/livemd/document/domain/BaseTimeEntity.java
@@ -15,8 +15,8 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    private LocalDateTime createdTime;
+    private LocalDateTime created_at;
 
     @LastModifiedDate
-    private LocalDateTime modifiedTime;
+    private LocalDateTime modified_at;
 }
--- server/spring-document/src/test/java/com/livemd/document/domain/UserDocumentsRepositoryTest.java
+++ server/spring-document/src/test/java/com/livemd/document/domain/UserDocumentsRepositoryTest.java
@@ -3,12 +3,14 @@ package com.livemd.document.domain;
 import com.livemd.document.domain.entity.UserDocuments;
 import com.livemd.document.domain.repository.UserDocumentsRepository;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +27,7 @@ public class UserDocumentsRepositoryTest {
         repository.deleteAll();
     }
 
-    @Test
+    @Test @Ignore
     public void saveAndFindDocuments(){
 
         String owner = "ownerTest";
@@ -41,6 +43,28 @@ public class UserDocumentsRepositoryTest {
         UserDocuments userDocuments = userDocumentsList.get(0);
         assertThat(userDocuments.getOwner()).isEqualTo(owner);
         assertThat(userDocuments.getUuid()).isEqualTo(uuid);
+    }
+
+    @Test
+    public void testBaseTimeEntity () {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019,10,20,0,0,0);
+        repository.save(UserDocuments.builder()
+                .owner("owner")
+                .uuid("uuid")
+                .build());
+
+        //when
+        List<UserDocuments> userDocumentsList = repository.findAll();
+
+        //then
+        UserDocuments userDocuments = userDocumentsList.get(0);
+
+        System.out.println(">>>>>> created_at" + userDocuments.getCreated_at() + ", modified_at" + userDocuments.getModified_at());
+
+        assertThat(userDocuments.getCreated_at()).isAfter(now);
+        assertThat(userDocuments.getModified_at()).isAfter(now);
+
     }
 
 
