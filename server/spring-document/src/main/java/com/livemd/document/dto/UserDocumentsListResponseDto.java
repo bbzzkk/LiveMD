d3d11c551f0ca8bd11f288a99abdfc384be8c6ce
soyoung <so_young_jung@naver.com>
2020-10-20 22:36:34 +0900

    CHORE : UserDocumentsListResponseDto에 uuid 컬럼 추가

--- server/spring-document/src/main/java/com/livemd/document/dto/UserDocumentsListResponseDto.java
+++ server/spring-document/src/main/java/com/livemd/document/dto/UserDocumentsListResponseDto.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 public class UserDocumentsListResponseDto {
     private Long id;
     private String owner;
+    private String uuid;
     private String title;
     private String content;
 
     public UserDocumentsListResponseDto(UserDocuments entity) {
         this.id = entity.getId();
         this.owner = entity.getOwner();
+        this.uuid = entity.getUuid();
         this.title = entity.getTitle();
         this.content = entity.getContent();
     }
