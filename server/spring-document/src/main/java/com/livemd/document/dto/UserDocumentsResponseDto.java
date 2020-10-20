699880a3d06b82f6584f149b5920c56cb707db7a
soyoung <so_young_jung@naver.com>
2020-10-20 19:54:36 +0900

    CHORE : UserDocumentsResponseDto 변수 추가 Why - 클라이언트에게 uuid를 넘겨줘야 content를 불러올 수 있기 때문 What - UserDocumentsResponseDto에 uuid 변수 추가

--- server/spring-document/src/main/java/com/livemd/document/dto/UserDocumentsResponseDto.java
+++ server/spring-document/src/main/java/com/livemd/document/dto/UserDocumentsResponseDto.java
@@ -8,12 +8,14 @@ public class UserDocumentsResponseDto {
 
     private Long id;
     private String owner;
+    private String uuid;
     private String title;
     private String content;
 
     public UserDocumentsResponseDto(UserDocuments entity){
         this.id = entity.getId();
         this.owner = entity.getOwner();
+        this.uuid = entity.getUuid();
         this.title = entity.getTitle();
         this.content = entity.getContent();
     }
