da9ae9101656ee1333c4130afe90d306d223db6a
soyoung <so_young_jung@naver.com>
2020-10-22 16:41:36 +0900

    LMD-126 FEATURE : 팀 특정 문서 조회 기능 구현

--- server/spring-document/src/main/java/com/livemd/document/controller/TeamDocumentsApiController.java
+++ server/spring-document/src/main/java/com/livemd/document/controller/TeamDocumentsApiController.java
@@ -1,13 +1,12 @@
 package com.livemd.document.controller;
 
+import com.livemd.document.domain.entity.TeamDocuments;
 import com.livemd.document.dto.TeamDocumentsListResponseDto;
+import com.livemd.document.dto.TeamDocumentsResponseDto;
 import com.livemd.document.dto.TeamDocumentsSaveRequestDto;
 import com.livemd.document.service.TeamDocumentsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,8 +21,13 @@ public class TeamDocumentsApiController {
         return service.create(requestDto);
     }
 
-    @GetMapping("/api/v1/user-documents")
+    @GetMapping("/api/v1/team-documents")
     public List<TeamDocumentsListResponseDto> findAll(){
         return service.findAll();
+    }
+
+    @GetMapping("/api/v1/team-documents/{id}")
+    public TeamDocumentsResponseDto findById(@PathVariable Long id){
+        return service.findById(id);
     }
 }
