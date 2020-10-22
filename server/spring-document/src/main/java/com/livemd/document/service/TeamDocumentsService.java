e508ea9f249c961b45d2788991ca38c4421edc48
soyoung <so_young_jung@naver.com>
2020-10-22 15:40:50 +0900

    LMD-122 FEATURE : 팀 문서 전체 조회 기능 추가

--- server/spring-document/src/main/java/com/livemd/document/service/TeamDocumentsService.java
+++ server/spring-document/src/main/java/com/livemd/document/service/TeamDocumentsService.java
@@ -1,10 +1,14 @@
 package com.livemd.document.service;
 
 import com.livemd.document.domain.repository.TeamDocumentsRepository;
+import com.livemd.document.dto.TeamDocumentsListResponseDto;
 import com.livemd.document.dto.TeamDocumentsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,4 +20,11 @@ public class TeamDocumentsService {
     public Long create(TeamDocumentsSaveRequestDto requestDto){
         return repository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public List<TeamDocumentsListResponseDto> findAll(){
+        return repository.findAll().stream().map(TeamDocumentsListResponseDto::new).collect(Collectors.toList());
+
+    }
+
 }
