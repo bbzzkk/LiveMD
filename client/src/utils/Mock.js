3eda3b6137a181b60d70931c76be776f51b08050
LeeYeonJu <yeonzzu2@gmail.com>
2020-10-23 03:47:01 +0900

    LMD-128 FEATURE : SideBar(1), document UI 구현

---
+++ client/src/utils/Mock.js
@@ -0,0 +1,23 @@
+export const documents = {
+  data: [
+    {
+      owner: 'yzz',
+      title: 'Spring Boot 정리',
+      createdAt: '2020-10-01 17:00',
+    },
+    {
+      owner: 'yzz',
+      title: 'Jenkins 정리',
+      createdAt: '2020-10-12 20:00',
+    },
+    {
+      owner: 'yzz',
+      title: 'Docker 정리',
+      createdAt: '2020-10-19 10:00',
+    },
+  ],
+};
+
+export default {
+  documents: documents,
+};
