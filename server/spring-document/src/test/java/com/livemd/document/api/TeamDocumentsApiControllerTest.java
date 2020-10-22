20f7f22ec74ea1890cf2d855955941d6f2cbfcc9
soyoung <so_young_jung@naver.com>
2020-10-22 13:51:43 +0900

    LMD-123 TEST : 팀 문서 생성 기능 테스트

---
+++ server/spring-document/src/test/java/com/livemd/document/api/TeamDocumentsApiControllerTest.java
@@ -0,0 +1,62 @@
+package com.livemd.document.api;
+
+import com.livemd.document.domain.entity.TeamDocuments;
+import com.livemd.document.domain.entity.UserDocuments;
+import com.livemd.document.domain.repository.TeamDocumentsRepository;
+import com.livemd.document.dto.TeamDocumentsSaveRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TeamDocumentsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TeamDocumentsRepository repository;
+
+    @After
+    public void clearAll() throws Exception {
+        repository.deleteAll();
+    }
+
+    @Test
+    public void createDocuments() throws Exception {
+
+        String owner = "owner";
+        String uuid = "uuid";
+        TeamDocumentsSaveRequestDto dto = TeamDocumentsSaveRequestDto.builder()
+                .owner(owner)
+                .uuid(uuid)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/team-documents";
+
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, dto, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<TeamDocuments> all = repository.findAll();
+        assertThat(all.get(0).getOwner()).isEqualTo(owner);
+        assertThat(all.get(0).getUuid()).isEqualTo(uuid);
+    }
+}
+
