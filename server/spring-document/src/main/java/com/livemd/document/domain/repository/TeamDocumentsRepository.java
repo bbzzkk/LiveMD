f5e4163cfca352ea170f87f0f9289a25497219d1
soyoung <so_young_jung@naver.com>
2020-11-02 14:13:18 +0900

    LMD-148 FIX : team 파트 모두 삭제

--- server/spring-document/src/main/java/com/livemd/document/domain/repository/TeamDocumentsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.livemd.document.domain.repository;
-
-import com.livemd.document.domain.entity.TeamDocuments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamDocumentsRepository extends JpaRepository<TeamDocuments, Long> {
-}
