8dfe5af331f06941be2e44ec20a59a02c4a35256
LeeYeonJu <yeonzzu2@gmail.com>
2020-11-02 15:36:02 +0900

    LMD-73 FEATURE : team crud 1차 test

--- server/express-team/routes/teams.js
+++ server/express-team/routes/teams.js
@@ -5,37 +5,38 @@ const Member = require("../models/Member");
 // Create Team
 router.post("/", async (req, res) => {
   try {
+    const { userId, teamname, description } = req.body;
     const isThereTeam = await Team.findOne({
-      teamname: req.body.teamname,
+      teamname: teamname,
     });
 
     if (isThereTeam) {
-      return res.json({
+      return res.status(500).json({
         result: false,
-        status: 409,
+        status: 500,
         error: "The name of team already exists.",
       });
     }
-
     const teamResult = await Team.create({
-      teamname: req.body.teamname,
-      description: req.body.description || "",
+      teamname: teamname,
+      description: description,
       createdAt: Date.now(),
       // thumbnailAttr,
     });
+
     await teamResult.save();
 
     const ownerResult = await Member.create({
-      teamId: teamResult.get(teamId),
-      userId: req.body.userId,
+      teamId: teamResult.teamId,
+      userId: userId,
       role: "owner",
-      // thumbnailAttr,
+      status: "active",
     });
     await ownerResult.save();
 
-    res.json({ result: true, status: 200 });
+    res.status(200).json({ result: true, status: 200 });
   } catch (e) {
-    res.json({
+    res.status(e.status).json({
       result: false,
       status: e.status,
       error: e.message,
@@ -43,50 +44,87 @@ router.post("/", async (req, res) => {
   }
 });
 
+// get all Team by userId
+router.get("/", async (req, res) => {
+  try {
+    const { userId } = req.query;
+    if (!userId)
+      res.status(500).json({
+        result: false,
+        status: 500,
+        error: "You must send userId by query parameter.",
+      });
+    const data = await Member.find({ userId: userId });
+    res.status(200).json({ result: true, data: data });
+  } catch (e) {
+    res.status(500).json({ result: false, error: e.message });
+  }
+});
+
 // get one Team by teamId
 router.get("/:teamId", async (req, res) => {
   try {
-    const result = await Team.findOne({ uid: req.params.teamId });
-    if (!result) {
-      return res.status(404).json({ result: false, error: "Team not found" });
+    // const member = await Member.findOne({
+    //   userId: req.query.userId,
+    //   teamId: req.params.teamId,
+    // });
+    const { teamId } = req.params;
+    const data = await Team.findOne({ teamId: teamId });
+    if (!data) {
+      return res
+        .status(404)
+        .json({ result: false, status: 404, error: "Team Not Found" });
     }
-    res.json({ result: true, result });
+    res.status(200).json({ result: true, status: 200, data: data });
   } catch (e) {
-    res.status(500).json({ result: false, error: e.message });
+    res
+      .status(e.status)
+      .json({ result: false, status: e.status, error: e.message });
   }
 });
 
 // Update by teamId
 router.put("/:teamId", async (req, res) => {
   try {
+    const { teamId, description, imageAttr } = req.body;
     // params uid
     // query imageURL
     // const imageURL = req.query.imageURL;
     // const result = await User.useFindAndModify({ uid: req.params.uid }, { dids: req.body.dids});
     const result = await await Team.update(
-      { teamId: req.params.teamId },
+      { teamId: teamId },
       { $push: { description: description } }
       // { $push: { imageURLs: imageURL } }
     );
     if (!result) {
-      return res.json({ result: false, status: 404, error: "User not found" });
+      return res
+        .status(404)
+        .json({ result: false, status: 404, error: "User Not Found" });
     }
-    res.json({ result: true });
+    res.status(200).json({ result: true, status: 200 });
   } catch (e) {
-    res.json({ result: false, status: e.status, error: e.message });
+    res
+      .status(e.status)
+      .json({ result: false, status: e.status, error: e.message });
   }
 });
 
 // Delete by teamId
 router.delete("/:teamId", async (req, res) => {
   try {
-    const result = await Team.findOneAndDelete({ teamId: req.params.teamId });
-    if (!result) {
-      return res.json({ result: false, status: 404, error: "Team not found" });
+    const { teamId } = req.params;
+    const teamResult = await Team.findOneAndDelete({ teamId: teamId });
+    if (!teamResult) {
+      return res
+        .status(404)
+        .json({ result: false, status: 404, error: "Team Not Found" });
     }
-    res.json({ result: true });
+    await Member.deleteMany({ teamId: teamId });
+    res.status(200).json({ result: true, status: 200 });
   } catch (e) {
-    res.json({ result: false, status: e.status, error: e.message });
+    res
+      .status(e.status)
+      .json({ result: false, status: e.status, error: e.message });
   }
 });
 
