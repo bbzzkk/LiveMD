702b2771ef5c049b2c98117121eab3fa593255c7
kbs <hot4514@gmail.com>
2020-11-06 19:19:06 +0900

    CHORE : chat 폴더 및 관련 코드 삭제

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import { Login, Main, OAuth2RedirectHandler, Chat } from '@/pages';
+import { Login, Main, OAuth2RedirectHandler } from '@/pages';
 import { Workspace } from '@/pages';
 import { LoadingIndicator } from '@/components/common';
 
@@ -14,39 +14,39 @@ import CreateRoom from '@/pages/VideoChat/CreateRoom';
 import Room from '@/pages/VideoChat/Room';
 
 const App = () => {
-  // const [authenticated, setAuthenticated] = useState(false);
-  // const [currentUser, setCurrentUser] = useState(null);
-  // const [loading, setLoading] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  // const loadCurrentlyLoggedInUser = () => {
-  //   setLoading(true);
+  const loadCurrentlyLoggedInUser = () => {
+    setLoading(true);
 
-  //   getCurrentUser()
-  //     .then(response => {
-  //       setCurrentUser(response);
-  //       setAuthenticated(true);
-  //       setLoading(false);
-  //     })
-  //     .catch(() => {
-  //       setLoading(false);
-  //     });
-  // };
+    getCurrentUser()
+      .then(response => {
+        setCurrentUser(response);
+        setAuthenticated(true);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
 
-  // const handleLogout = e => {
-  //   localStorage.removeItem('accessToken');
-  //   setAuthenticated(false);
-  //   setCurrentUser(null);
-  //   alert('로그아웃 되었습니다.');
-  //   e.preventDefault();
-  // };
+  const handleLogout = e => {
+    localStorage.removeItem('accessToken');
+    setAuthenticated(false);
+    setCurrentUser(null);
+    alert('로그아웃 되었습니다.');
+    e.preventDefault();
+  };
 
-  // useEffect(() => {
-  //   loadCurrentlyLoggedInUser();
-  // }, []);
+  useEffect(() => {
+    loadCurrentlyLoggedInUser();
+  }, []);
 
-  // if (loading) {
-  //   return <LoadingIndicator />;
-  // }
+  if (loading) {
+    return <LoadingIndicator />;
+  }
 
   return (
     <>
@@ -56,7 +56,6 @@ const App = () => {
           <Route exact path="/" component={Main} />
           <Route path="/login" component={Login} />
           <Route path="/oauth2/redirect" component={OAuth2RedirectHandler} />
-          {/* <Route path="/chat" component={Chat} /> */}
           <Route path="/create" component={CreateRoom} />
           <Route path="/room/:roomID" component={Room} />
         </Switch>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,4 +2,3 @@ export { default as Main } from './Home';
 export { default as Workspace } from './Workspace';
 export { default as OAuth2RedirectHandler } from './OAuth2RedirectHandler';
 export { default as Login } from './Login';
-export { default as Chat } from './Chat';
