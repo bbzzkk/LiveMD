3020beb69ea144577cdb9c50e067d6f6c2741c60
soyoung <so_young_jung@naver.com>
2020-11-11 15:00:12 +0900

    LMD-155 FEATURE : exception handling 완료

--- server/spring-document/src/main/java/com/livemd/document/exception/GlobalExceptionHandler.java
+++ server/spring-document/src/main/java/com/livemd/document/exception/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.livemd.document.exception;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(value=RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value= NoSuchElementException.class)
     public ErrorMessage handleArgumentException(){
+        return new ErrorMessage(404, false);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = RuntimeException.class)
+    public ErrorMessage handleDupilcateException(){
         return new ErrorMessage(500, false);
     }
 }
