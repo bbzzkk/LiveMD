92cca97ed5e3dd94f45bcc11af2c1b4dae0ca261
kwak-bs <iam-bs@naver.com>
2020-11-23 02:10:53 +0900

    LMD-170 FIX : block 1 버튼 가운데 정렬 완성 STYLE : block2 까지 완성 block2 버튼을 동그랗게 만드는 것은 불가능 , block4 작업중

--- client/src/components/home/Body/Body.jsx
+++ client/src/components/home/Body/Body.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import S from './style';
 import { Button } from '@material-ui/core';
+import { withStyles} from '@material-ui/core/styles';
 import Fade from 'react-reveal/Fade';
 
 const styles = {
@@ -8,6 +9,7 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
     width: '100%',
     height: '100%',
     background: '#000',
@@ -17,7 +19,7 @@ const styles = {
   title: {
     position: 'relative',
     textAlign: 'center',
-    fontSize: 40,
+    fontSize: 60,
     color: '#fff',
     fontFamily:
       'SF Pro KR,SF Pro Display,SF Pro Icons,Apple Gothic,HY Gulim,MalgunGothic,HY Dotum,Lexi Gulim,Helvetica Neue,Helvetica,Arial,sans-serif',
@@ -35,25 +37,69 @@ const styles = {
     lineHeight: 'normal',
   },
 
-  button : {
-    fontSize: 17,
-    color: '#fff',
-    fontFamily:
-      'SF Pro KR,SF Pro Display,SF Pro Icons,Apple Gothic,HY Gulim,MalgunGothic,HY Dotum,Lexi Gulim,Helvetica Neue,Helvetica,Arial,sans-serif',
-    border: '1px solid #f5f5f7',
-    borderRadius: '18px',
-    fontWeight: 'bold',
+  span_LiveMD : {
+    color: 'white',
+    fontSize: 40,
   },
 };
 
+const ColorButton1 = withStyles(() => ({
+  root: {
+    fontSize: 19,
+    fontFamily:
+      'SF Pro KR,SF Pro Display,SF Pro Icons,Apple Gothic,HY Gulim,MalgunGothic,HY Dotum,Lexi Gulim,Helvetica Neue,Helvetica,Arial,sans-serif',
+    border: '2px solid #f5f5f7',
+    borderRadius: '30px',
+    color: '#fff',
+    backgroundColor: '#000',
+    fontWeight: 'bold',
+    minWidth: '28px',
+    width: '300px',
+    bottom: '-150px',
+    paddingTop: '10px',
+
+    '&:hover': {
+      backgroundColor: '#fff',
+      color: '#000',
+    },
+  },
+}))(Button);
+
+const ColorButton2 = withStyles(() => ({
+  root: {
+    fontSize: 19,
+    fontFamily:
+      'SF Pro KR,SF Pro Display,SF Pro Icons,Apple Gothic,HY Gulim,MalgunGothic,HY Dotum,Lexi Gulim,Helvetica Neue,Helvetica,Arial,sans-serif',
+    border: '2px solid',
+    borderImage: 'linear-gradient(to right, #005e7f, #61b6cd)',
+    borderImageSlice: 1,
+    borderImageWidth: 3,
+    color: '#fff',
+    background: '#000',
+    fontWeight: 'bold',
+    minWidth: '28px',
+    width: '150px',
+    bottom: '-100px',
+    paddingTop: '10px',
+
+    '&:hover': {
+      backgroundColor: '#fff',
+      color: '#000',
+    },
+  },
+}))(Button);
+
 const block1 = () => {
+
   return (
     <>
       <h2 style={styles.title}>LiveMD</h2>
-      <S.col_1_1>협업의 즐거움.</S.col_1_1>
-      <Button variant="outlined" style={styles.button}>
-        구글 로그인하기
-      </Button> 
+      
+      <S.ment1_sc>협업의 즐거움.</S.ment1_sc>
+
+      <ColorButton1 variant="contained" >
+        구글 로그인으로 시작하기
+      </ColorButton1>
     </>
   );
 };
@@ -65,18 +111,46 @@ const block2 = () => {
       Untact 시대에도 어김없이 찾아오는 회의 시간.<br/>
       같은 공간에 없어도 함께 있는 것 같은 편안함.<br/>
       동료들과의 소통. 동시에 이루어지는 문서 작업.<br/><br/>
-      서로의 얼굴, 목소리를 확인하며 채팅까지 가능한 소통 기능.<br/> 
-      마크다운을 활용하여 문서를 작업할 수 있는 협업 기능.<br/>
+      <S.ment2_sc>서로의 얼굴, 목소리를 확인하며 채팅까지 가능한 소통 기능.<br/> 
+      마크다운을 활용하여 문서를 작업할 수 있는 협업 기능.<br/></S.ment2_sc>
       이 모든 것의 집합체가 보여주는 놀라운 능력을<br/>
-      여기 LiveMD가 소개합니다.
+      여기 <span style={styles.span_LiveMD}>LiveMD</span>가 소개합니다.
       </h2>
+      
+      <a href="#howTo">
+      <ColorButton2 variant="contained" >
+          가이드
+      </ColorButton2>
+      </a>
     </>
   );
 };
 
-const animateList = [block1(), block2(), '둠칫칫칫', '둠둠칫칫', '둠칫칫둠'];
+
+const block4 = () => {
+  return (
+    <>
+      <h2 style={styles.ment1} id="howTo">
+      Untact 시대에도 어김없이 찾아오는 회의 시간.<br/>
+      같은 공간에 없어도 함께 있는 것 같은 편안함.<br/>
+      동료들과의 소통. 동시에 이루어지는 문서 작업.<br/><br/>
+      <S.ment2_sc>서로의 얼굴, 목소리를 확인하며 채팅까지 가능한 소통 기능.<br/> 
+      마크다운을 활용하여 문서를 작업할 수 있는 협업 기능.<br/></S.ment2_sc>
+      이 모든 것의 집합체가 보여주는 놀라운 능력을<br/>
+      여기 <span style={styles.span_LiveMD}>LiveMD</span>가 소개합니다.
+      </h2>
+
+      <ColorButton2 variant="contained" >
+          가이드
+      </ColorButton2>
+    </>
+  );
+};
+
+const animateList = [block1(), block2(), '둠칫칫칫', block4(), '둠칫칫둠'];
 
 const Body = () => (
+  
   <Fragment>
     {animateList.map((item, key) => (
       <div style={styles.block} key={key}>
--- client/src/components/home/Body/style.jsx
+++ client/src/components/home/Body/style.jsx
@@ -1,75 +1,22 @@
 import styled from 'styled-components';
 
 export default {
-  //col1
-  col_1: styled.div`
-    width: 100%;
-    height: 400px;
-    display: flex;
-    background-color: #000000;
-  `,
-  col_1_1: styled.h2`
+  //block1
+  ment1_sc: styled.h2`
     color: transparent;
-    background: -webkit-gradient(linear,left top,right top,from(#ff7a1b),color-stop(#de3fe3),to(#22dcbd));
     background: linear-gradient(90deg,#ff7a1b,#de3fe3,#22dcbd);
-    background-clip: text;
     -webkit-background-clip: text;
-    font-size: 60px; 
+    font-size: 100px; 
     font-weight: bold;
     line-height: initial;
+    text-align: center;
   `,
-  col_1_2: styled.div`
-  color: white;
-  font-size: 1.3em; 
-  text-align-last: center;
-  padding-top: 15px;
+
+  ment2_sc: styled.h2`
+  color: transparent;
+  background: linear-gradient(to right, #005e7f, #61b6cd);
+  -webkit-background-clip: text;
+  font-weight: bold;
+  font-size: inherit;
 `,
-  col_1_3: styled.div`
-  position : relative;
-  top : 65px;
-  `,
-
-  col_1_button: styled.button`
-    background: black;
-    color: #fff;
-    border: 1px solid;
-  `,
-
-  //col2
-  col_2: styled.div`
-  width: 100%;
-  height: 600px;
-  align-items: center;
-  justify-content: center;
-  background-color: #ffffff;
-  color: white;
-`,
-  col_2_1: styled.div`
-
-  `,
-  col_2_2: styled.div`
-
-  `,
-  col_2_3: styled.div`
-
-  `,
-
-  //col3
-  col_3: styled.div`
-  width: 100%;
-  height: 500px;
-  align-items: center;
-  justify-content: center;
-  background-color: #bbbbbb;
-  color: white;
-  `,
-  col_2_1: styled.div`
-
-  `,
-  col_2_2: styled.div`
-
-  `,
-  col_2_3: styled.div`
-
-  `,
 };
