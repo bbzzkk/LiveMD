a514c5fc2105b4469ece43c08e8653ed68246e09
kwak-bs <iam-bs@naver.com>
2020-11-25 13:20:25 +0900

    LMD-170 STYLE : block4 carousel 구현중

--- client/src/components/home/Body/Block4.jsx
+++ client/src/components/home/Body/Block4.jsx
@@ -1,24 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+import styled from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-export default class SimpleSlider extends Component {
-  render() {
+const Container = styled.div`
+  width: 4312px;
+  height: 70vh;
+  margin: 0 auto;
+  background-color: white;
+  text-align: center;
+`;
+
+const GuideName = styled.h1`
+  text-align: center;
+  //font-size: 32px;
+  color: white;
+  font-family:
+        'SF Pro KR,SF Pro Display,SF Pro Icons,Apple Gothic,HY Gulim,MalgunGothic,HY Dotum,Lexi Gulim,Helvetica Neue,Helvetica,Arial,sans-serif';
+  //font-weight: 600;
+  line-height: normal;
+`;
+
+const StyledSlider = styled(Slider)`
+    .slick-slide div{
+      outline: none;
+    }
+`;
+
+const ImageContainer = styled.div`
+  margin: 0 16px;
+  background-color: yellow;
+  max-height: 100px;
+`;
+
+const Image = styled.img`
+// max-width:100%;
+// max-height:100%;
+`;
+
+const Block4 = () => {
+
     const settings = {
       dots: true,
-      infinite: false,
-      speed: 500,
+      infinite: true,
+      speed: 1000,
       slidesToShow: 1,
       slidesToScroll: 1,
-      arrows: true,
+      arrows: false,
+      fade: true,
+      lazyLoad: true,
+      
     };
+
     return (
-      <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
+      <>
+      <GuideName id='howTo'>가이드</GuideName>
+      
+      <Container
+      >
+        <StyledSlider {...settings}>
           <div>
-            <h1>1</h1>
+            <h1>1. 구글 로그인을 진행합니다. </h1>
+            <ImageContainer>
+            <Image className="logo-img" src="/images/temp.jpg" alt="logo" />
+            </ImageContainer>
           </div>
           <div>
             <h3>2</h3>
@@ -29,14 +75,11 @@ export default class SimpleSlider extends Component {
           <div>
             <h3>4</h3>
           </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
+        </StyledSlider>
+      </Container>
+      </>
     );
   }
-}
+
+
+export default Block4;
