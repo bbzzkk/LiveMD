1385708503be029fc73f13e6c57d83af71ecb21c
LeeYeonJu <yeonzzu2@gmail.com>
2020-11-25 18:34:27 +0900

    LMD-185 CHORE : uuid 생성하는 함수 추가

--- client/src/utils/uuid.js
+++ client/src/utils/uuid.js
@@ -1,7 +1,7 @@
-import uuid4 from 'uuid4';
+import { v4 as uuid } from 'uuid';
 
 const getUuid = () => {
-  const tokens = uuid4().split('-');
+  const tokens = uuid().split('-');
   return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
 };
 
