5d4926e0228b0e688fef603477756d4bf3445abc
kwak-bs <iam-bs@naver.com>
2020-11-25 13:03:56 +0900

    LMD-170 STYLE : Footer 스타일 완료

--- client/src/components/home/Footer/style.jsx
+++ client/src/components/home/Footer/style.jsx
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 export default {
   Footer: styled.div`
     width: 100%;
-    height: 100px;
-    display: flex;
+    //height: 25%;
+    //display: flex;
     align-items: center;
     justify-content: center;
     background-color: #000000;
     color: white;
+    padding-top: 3em;
+    //position: relative;
+    bottom: 0;
   `,
 };
