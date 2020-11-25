104fa0ec2f190fee953fe1384efbdecf6c5c442f
LeeYeonJu <yeonzzu2@gmail.com>
2020-11-25 18:01:14 +0900

    LMD-185 FEATURE : store 추가 What - model: user, board, team, member - store: AuthStore, BoardStore, PaginationStore, RootStore

--- client/src/stores/BoardStore.js
+++ client/src/stores/BoardStore.js
@@ -9,58 +9,25 @@ const BoardStore = types
   .model('BoardStore', {
     board: types.maybe(Board),
   })
-  .actions(self => {
-    let controller;
-    return {
-      // afterCreate() {
-      //   self.load();
-      // },
-      // load: flow(function* () {
-      //   controller = window.AbortController && new window.AbortController();
-      //   try {
-      //     const response = yield window.fetch(
-      //       `http://localhost:3010/documents`,
-      //       {
-      //         signal: controller.signal,
-      //       },
-      //     );
-      //     const documents = yield response.json().data;
-      //     applySnapshot(self.documents, documents);
-      //     console.log('success');
-      //   } catch (error) {
-      //     console.log('failed: ', error);
-      //   }
-      // }),
-      // reload() {
-      //   console.log('reload');
-      //   if (controller) controller.abort();
-      //   self.load();
-      // },
-      // beforeDestroy() {
-      //   if (controller) controller.abort();
-      // },
-      createDocument: flow(function* () {
-        const documentId = getUuid();
-        // console.log(`owner:: ${self.board.owner}`);
-        const ownerId = self.board.owner.id;
-        // console.log(`documentId:: ${documentId}`);
-        // console.log(`ownerId:: ${ownerId}`);
-
-        yield api
-          .post(`http://localhost:3010/documents?${ownerId}`, {
-            id: documentId,
-          })
-          // .get(`http://localhost:3010/documents`)
-          .then(response => {
-            self.board.addDocument('123');
-            // } else console.log('server error');
-          })
-          .catch(e => {
-            console.log('catch 문 들어옴');
-            console.log(e);
-          });
-      }),
-    };
-  });
+  .actions(self => ({
+    setBoard(board) {
+      self.board = board;
+    },
+    createDocument: flow(function* () {
+      const documentId = getUuid();
+      const ownerId = self.board.owner.id;
+      yield api
+        .post(`http://localhost:3010/documents?${ownerId}`, {
+          id: documentId,
+        })
+        .then(() => {
+          self.board.addDocument(documentId);
+        })
+        .catch(e => {
+          console.log('catch 문 들어옴');
+          console.log(e);
+        });
+    }),
+  }));
 
 export default BoardStore;
--- client/src/stores/models/User.js
+++ client/src/stores/models/User.js
@@ -1,9 +1,13 @@
 import { types } from 'mobx-state-tree';
 
+import Board from './Board';
+
 export const UserBase = types.model('UserBase', {
   id: types.identifier,
-  name: types.string,
+  username: types.string,
   email: types.string,
+  board: types.maybe(Board),
+  // board: types.maybe(types.reference(types.late(() => Board))),
 });
 
 const User = UserBase.named('User').actions(self => ({
