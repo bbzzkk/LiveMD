9f6557baa77ad2e901b9f4941ff042da535a7069
kwak-bs <iam-bs@naver.com>
2020-11-26 18:07:46 +0900

    LMD-170 STYLE : Workspace people 작업중

---
+++ client/src/components/workspace/PeopleMain/PeopleMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PeopleMain = props => {
+  return (
+    <>
+      <Documents></Documents>
+    </>
+  );
+};
+
+export default PeopleMain;
