e10252f28c6271ddc4809643c5d79b284baf5d98
kwak-bs <iam-bs@naver.com>
2020-11-27 21:23:09 +0900

    LMD-170 STYLE : workspace people 작업중

--- client/src/components/workspace/PeopleMain/PeopleMain.jsx
+++ client/src/components/workspace/PeopleMain/PeopleMain.jsx
@@ -1,76 +1,183 @@
 import React from 'react';
-
+import S from './style';
+import { Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const PeopleMain = () => {
-
   return (
-    <>
-      <div className="manage section col-sm-9">
-        <h3>Members</h3>
-        <form class="form-horizontal team-member" >
-          <div className="form-group add-member">
-            <label for="inputAddNewMember" className="col-xs-12 control-label pl-0">Add New Member</label>
-            <div className="col-sm-8 col-xs-12 pl-0">
-              <div className="select2-container select2-container-multi form-control search-user" id="s2id_inputAddNewMember">
-                <ul className="select2-choices">
-                  <li classNmae="select2-search-field">
-                    <label for="s2id_autogen1" className="sr-only select2-offscreen">Add New Member</label>
-                    <input type="text" autoComplete="member" autoCorrect="off" autoCapitalize="off" 
-                    spellCheck="false" className="form-control select2-input select2-default" id="s2id_autogen1"
-                    placeholder aria-activedescendant="select2-result-label-48"/>
-                  </li>
-                </ul>
+    <S.container className="container">
+      <h3 className="h3Member">Member</h3>
+      <form className="form-horizontal" role="form">
+        {/* Add New Member */}
+        <div className="form-group">
+          <label for="inputAddNewMember">Add New Member</label>
+          <input
+            type="text"
+            class="form-control search-user"
+            id="inputAddNewMember"
+            placeholder="Username or email address"
+          />
+          <div className="dropdown select">
+            <button
+              class="btn btn-default dropdown-toggle"
+              type="button"
+              id="teamRoleMenu"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <span className="dropdown-label pull-left">Admin</span>
+              <span class="caret"></span>
+            </button>
+            <ul
+              className="dropdown-menu role-dropdown"
+              aria-labelledby="teamRoleMenu"
+              role="menu"
+            >
+              <li>
+                <a className="dropdown-item">
+                  <div>Admin</div>
+                  <small>
+                    "Admin can manage members and billings (with billing role),
+                    and read and write notes."
+                  </small>
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item">
+                  <div>Writer</div>
+                  <small>"Writer can read and write all team notes."</small>
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item">
+                  <div>Reader</div>
+                  <small>"Reader can read all team notes."</small>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <Button variant="outlined">Add</Button>
+        </div>
+        <br />
+        <br />
+        {/* Member List */}
+        <div className="form-group">
+          <div className="member-list">
+            {/* 본인 */}
+            <div className="item clearfix">
+              <span className="id" style={{ display: 'none' }}>
+                7104fdbc-c0e9-4c5a-af04-62607ef52db2
+              </span>
+              <div className="col-sm-8 col-xs-12">
+                <img
+                  className="ui-avatar circle"
+                  width="20"
+                  height="20"
+                  src="https://avatars.githubusercontent.com/u/51367622?s=96"
+                  style={{ marginTop: '-4px' }}
+                />
+                <span className="ui-member-name">kwak-bs</span>
+                <i
+                  className="fa fa-envelope-o ui-resend-invitation"
+                  aria-hidden="true"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="Resend Invitation"
+                  style={{ cursor: 'pointer' }}
+                />
               </div>
-              <input type="text" className="form-control search-user" id="inputAddNewMember"
-              placeholder="Username or email address" tabIndex="-1" /> 
-            </div>
-            <div className="col-sm-2 col-xs-6 col-role">
-              <div className="dropdown select">
-                <button className="btn btn-default dropdown-toggle" type="button" id="teamRoleMenu"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
-                  <span className="dropdown-label pull-left">Admin</span>
-                  <span className="bs-caret">
-                    <span className="caret"></span>
-                  </span>
-                </button>
-                <ul className="dropdown-menu role-dropdown" aria-labelledby="teamRoleMenu" >
-                  <li>
-                    <a>
-                      <div>Admin</div>
-                      <small>"Admin can manage members and billings (with billing role), and read and write notes."</small>
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      <div>Writer</div>
-                      <small>"Writer can read and write all team notes."</small>
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      <div>Reader</div>
-                      <small>"Reader can read all team notes."</small>
-                    </a>
-                  </li>
-                </ul>
+              <div className="col-sm-2 col-xs-6 col-role">
+                <div className="dropdown select" style={{ minWidth: '100%' }}>
+                  <button
+                    className="btn btn-default dropdown-toggle no-border disabled"
+                    type="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="true"
+                  >
+                    <span className="dropdown-label pull-left">Owner</span>
+                  </button>
+                </div>
               </div>
             </div>
-            <div className="col-sm-2 col-xs-6 col-add pl-0-sm">
-              <a className="btn btn-block btn-default ui-member-add">
-                Add
-              </a>
+            <hr />
+
+            {/* 동료*/}
+            <div className="item clearfix">
+              <span className="id" style={{ display: 'none' }}>
+                f21c2d60-7be8-4d97-a2f9-311e88819f13
+              </span>
+              <div className="col-sm-8 col-xs-12">
+                <img
+                  className="ui-avatar circle"
+                  width="20"
+                  height="20"
+                  src="https://www.gravatar.com/avatar/d17318498c5efa6d2eeeab9c13ae92eb?s=96"
+                  style={{ marginTop: '-4px' }}
+                />
+                <span className="ui-member-name">soyoung</span>
+                <i
+                  className="fa fa-envelope-o ui-resend-invitation"
+                  aria-hidden="true"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="Resend Invitation"
+                  style={{ cursor: 'pointer' }}
+                />
+              </div>
+              <div className="col-sm-2 col-xs-6 col-role">
+                <div className="dropdown select" style={{ minWidth: '100%' }}>
+                  <button
+                    className="btn btn-default dropdown-toggle no-border"
+                    type="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="true"
+                  >
+                    <span className="dropdown-label pull-left">Admin</span>
+                  </button>
+                  <ul
+                    className="dropdown-menu role-dropdown"
+                    aria-labelledby="teamRoleMenu"
+                    role="menu"
+                  >
+                    <li>
+                      <a className="dropdown-item">
+                        <div>Admin</div>
+                        <small>
+                          "Admin can manage members and billings (with billing
+                          role), and read and write notes."
+                        </small>
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">
+                        <div>Writer</div>
+                        <small>
+                          "Writer can read and write all team notes."
+                        </small>
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">
+                        <div>Reader</div>
+                        <small>"Reader can read all team notes."</small>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-sm-2 col-xs-6 text-right col-delete">
+                <span className="ui-member-delete">
+                  <DeleteIcon />
+                </span>
+              </div>
             </div>
           </div>
-          <br/>
-          <div className="form-group">
-            <div className="member-list">
-
-            </div>
-
-          </div>
-        </form>
-      </div>
-    </>
+        </div>
+      </form>
+    </S.container>
   );
 };
 
