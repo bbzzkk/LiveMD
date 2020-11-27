6e6718ef5cfa0e8a6a890a0f06c4e34dd0c291d2
kwak-bs <iam-bs@naver.com>
2020-11-27 21:32:53 +0900

    LMD-170 STYLE : workspace people css

---
+++ client/src/components/workspace/PeopleMain/style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const S = {
+  container: styled.div`
+    text-align: inherit;
+
+    .h3Member {
+      font-weight: 600;
+      font-size: 22px;
+      padding-bottom: 12px;
+      margin-top: 0;
+      border-bottom: 2px solid #4f4f4f;
+    }
+  `,
+};
+
+export default S;
