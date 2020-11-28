6c958b588d6f35a834ded948178d4cffe977c90c
LeeYeonJu <yeonzzu2@gmail.com>
2020-11-28 12:25:32 +0900

    LMD-194 FEATURE : 워크스페이스에서 로그아웃 처리

--- client/src/components/workspace/Header/Others/Others.jsx
+++ client/src/components/workspace/Header/Others/Others.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
 
-const Others = () => {
+import S from './style';
+
+const Others = props => {
+  const { signOut } = props.store.authStore;
+  const handleSignout = async () => {
+    await signOut().then(() => props.history.push('/'));
+  };
   return (
-    <div>설정/알림 아이콘</div>
-  )
-}
+    <S.SignOutButton
+      variant="contained"
+      color="primary"
+      size="small"
+      startIcon={<S.ExitIcon />}
+      style={{ marginTop: 'auto', marginLeft: '-34%' }}
+      onClick={handleSignout}
+    >
+      Sign out
+    </S.SignOutButton>
+  );
+};
 
-export default Others;
+export default withRouter(inject('store')(observer(Others)));
