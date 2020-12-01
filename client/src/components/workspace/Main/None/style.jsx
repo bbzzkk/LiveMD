f8bf585910524e31e36bd73fa35b91ef29a21a7e
kwak-bs <iam-bs@naver.com>
2020-12-01 21:00:14 +0900

    LMD-170 STYLE : NONE 수정

--- client/src/components/workspace/Main/None/style.jsx
+++ client/src/components/workspace/Main/None/style.jsx
@@ -46,27 +46,10 @@ import Button from '@material-ui/core/Button';
 
 const S = {
   Container: styled.div`
-    display: flex;
+    min-width: 10em;
     text-align: center;
-    display: flex;
-  `,
-  Horizon: styled.h1`
-    width: 100%;
-    text-align: center;
-    border-bottom: 1px solid #9fbedf;
-    line-height: 0.1em;
-    margin: 10px 0 20px;
-  `,
-  Text: styled.span`
-    background: #fff;
-    padding: 0 10px;
-    // font-size: 50%;
-  `,
-  DocumentContainer: styled.div`
     position: relative;
-    background: yellow;
-    width: 100%;
-    height: fit-content;
+    top: 15rem;
   `,
 };
 
